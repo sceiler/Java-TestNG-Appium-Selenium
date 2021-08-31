0bca1c2ce37e7480da00cec70407ad77fb4377cd
Yi Min Yang <yi-min.yang@saucelabs.com>
2021-08-31 13:35:59 +0200

    bla

---
+++ src/test/java/com/saucelabs/yy/Tests/Appium/SwagLabsApp/EmulatorSimulator/AddItemsTests.java
@@ -0,0 +1,30 @@
+package com.saucelabs.yy.Tests.Appium.SwagLabsApp.EmulatorSimulator;
+
+import io.appium.java_client.MobileBy;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+
+public class AddItemsTests extends TestBase {
+
+    @Test(dataProvider = "EmulatorSimulatorDataProvider")
+    public void addOneItemtoCart(String platform, String deviceName, String platformVersion, Method methodName) throws MalformedURLException {
+        createDriver(platform, deviceName, platformVersion, methodName.getName());
+        getDriver().findElement(new MobileBy.ByAccessibilityId("test-Username")).sendKeys("standard_user");
+        getDriver().findElement(new MobileBy.ByAccessibilityId("test-Password")).sendKeys("secret_sauce");
+        getDriver().findElement(new MobileBy.ByAccessibilityId("test-LOGIN")).click();
+
+        if (platform.equalsIgnoreCase("Android")) {
+            getDriver().findElement(By.xpath("(//android.view.ViewGroup[@content-desc=\"test-ADD TO CART\"])[1]")).click();
+            getDriver().findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"test-Cart\"]/android.view.ViewGroup/android.widget.TextView")).click();
+            Assert.assertEquals(getDriver().findElement(By.xpath("(//android.view.ViewGroup[@content-desc=\"test-Description\"])[1]/android.widget.TextView[1]")).getText(), "Sauce Labs Backpack");
+        } else {
+            getDriver().findElement(By.xpath("(//XCUIElementTypeOther[@name=\"test-ADD TO CART\"])[1]")).click();
+            getDriver().findElement(By.xpath("(//XCUIElementTypeOther[@name=\"1\"])[4]")).click();
+            Assert.assertTrue(getDriver().findElement(By.xpath("(//XCUIElementTypeOther[@name=\"1\"])[4]")).isDisplayed());
+        }
+    }
+}
---
+++ src/test/java/com/saucelabs/yy/Tests/Appium/SwagLabsApp/EmulatorSimulator/LoginTests.java
@@ -0,0 +1,46 @@
+package com.saucelabs.yy.Tests.Appium.SwagLabsApp.EmulatorSimulator;
+
+import io.appium.java_client.MobileBy;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+
+public class LoginTests extends TestBase {
+
+    @Test(dataProvider = "EmulatorSimulatorDataProvider")
+    public void invalidCredentials(String platform, String deviceName, String platformVersion, Method methodName) throws MalformedURLException {
+        createDriver(platform, deviceName, platformVersion, methodName.getName());
+        getDriver().findElement(new MobileBy.ByAccessibilityId("test-Username")).sendKeys("bad");
+        getDriver().findElement(new MobileBy.ByAccessibilityId("test-Password")).sendKeys("bad");
+        getDriver().findElement(new MobileBy.ByAccessibilityId("test-LOGIN")).click();
+
+        Assert.assertTrue(getDriver().findElement(new MobileBy.ByAccessibilityId("test-Error message")).isDisplayed());
+    }
+
+    @Test(dataProvider = "EmulatorSimulatorDataProvider")
+    public void blankCredentials(String platform, String deviceName, String platformVersion, Method methodName) throws MalformedURLException {
+        createDriver(platform, deviceName, platformVersion, methodName.getName());
+        getDriver().findElement(new MobileBy.ByAccessibilityId("test-Username")).sendKeys("");
+        getDriver().findElement(new MobileBy.ByAccessibilityId("test-Password")).sendKeys("");
+        getDriver().findElement(new MobileBy.ByAccessibilityId("test-LOGIN")).click();
+
+        Assert.assertTrue(getDriver().findElement(new MobileBy.ByAccessibilityId("test-Error message")).isDisplayed());
+    }
+
+    @Test(dataProvider = "EmulatorSimulatorDataProvider")
+    public void validCredentials(String platform, String deviceName, String platformVersion, Method methodName) throws MalformedURLException {
+        createDriver(platform, deviceName, platformVersion, methodName.getName());
+        getDriver().findElement(new MobileBy.ByAccessibilityId("test-Username")).sendKeys("standard_user");
+        getDriver().findElement(new MobileBy.ByAccessibilityId("test-Password")).sendKeys("secret_sauce");
+        getDriver().findElement(new MobileBy.ByAccessibilityId("test-LOGIN")).click();
+
+        if (platform.equalsIgnoreCase("Android")) {
+            Assert.assertTrue(getDriver().findElement(By.xpath("(//android.view.ViewGroup[@content-desc=\"test-ADD TO CART\"])[1]")).isDisplayed());
+        } else {
+            Assert.assertTrue(getDriver().findElement(By.xpath("(//XCUIElementTypeOther[@name=\"test-ADD TO CART\"])[1]")).isDisplayed());
+        }
+    }
+}
