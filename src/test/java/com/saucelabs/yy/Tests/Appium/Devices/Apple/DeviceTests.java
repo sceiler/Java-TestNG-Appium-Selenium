5f6eac6f69e39c616f7375310b4a1100091542e4
Yi Min Yang <yi-min.yang@saucelabs.com>
2021-09-02 17:08:00 +0200

    te

--- src/test/java/com/saucelabs/yy/Tests/Appium/Devices/Apple/DeviceTests.java
+++ src/test/java/com/saucelabs/yy/Tests/Appium/Devices/Apple/DeviceTests.java
@@ -1,9 +1,12 @@
 package com.saucelabs.yy.Tests.Appium.Devices.Apple;
 
+import org.openqa.selenium.OutputType;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
+import java.util.Locale;
 
 public class DeviceTests extends TestBase {
 
@@ -15,8 +18,8 @@ public class DeviceTests extends TestBase {
         createDriver(platform, deviceName, platformVersion, methodName.getName());
 
         driver.get().activateApp("com.apple.Preferences");
-        //String result = ocr.get().getText(driver.get().getScreenshotAs(OutputType.FILE)).toLowerCase(Locale.ROOT);
+        String result = ocr.get().getText(driver.get().getScreenshotAs(OutputType.FILE)).toLowerCase(Locale.ROOT);
 
-        //Assert.assertTrue(result.contains("sign in to") || result.contains("testobject"), deviceName + System.lineSeparator() + result);
+        Assert.assertTrue(result.contains("sign in to") || result.contains("testobject"), deviceName + System.lineSeparator() + result);
     }
 }
