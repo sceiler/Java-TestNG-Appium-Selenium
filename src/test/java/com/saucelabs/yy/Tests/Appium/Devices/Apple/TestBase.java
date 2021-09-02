674bc9b4034bbb2982cf6f60007fc204e4ba9121
Yi Min Yang <yi-min.yang@saucelabs.com>
2021-09-02 18:00:57 +0200

    rtgh

--- src/test/java/com/saucelabs/yy/Tests/Appium/Devices/Apple/TestBase.java
+++ src/test/java/com/saucelabs/yy/Tests/Appium/Devices/Apple/TestBase.java
@@ -72,6 +72,7 @@ public class TestBase extends SuperTestBase {
         capabilities.setCapability("deviceName", deviceName);
         capabilities.setCapability("name", methodName);
         capabilities.setCapability("testobject_session_creation_timeout", "90000");
+        capabilities.setCapability("newCommandTimeout", "90000");
 
         if (platformName.equals("Android")) {
             capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
