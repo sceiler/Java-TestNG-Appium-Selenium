e27a040c78bc160dc3f4d9845dd429eac3c14c87
Yi Min Yang <yi-min.yang@saucelabs.com>
2022-04-13 23:08:51 +0200

    buenas those-things.

---
+++ src/test/java/com/saucelabs/yy/Tests/Selenium/SimpleTestsReallyLong/OpenURLTests.java
@@ -0,0 +1,17 @@
+package com.saucelabs.yy.Tests.Selenium.SimpleTestsReallyLong;
+
+import com.saucelabs.yy.Tests.Selenium.TestBase;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+
+public class OpenURLTests extends TestBase {
+
+    @Test(dataProvider = "Browsers", invocationCount = 40)
+    public void openSauceDemo(String browser, String version, String os, Method method) throws MalformedURLException {
+        createDriver(browser, version, os, method.getName());
+        Assert.assertTrue(getRemoteWebDriver().getTitle().contains("Swag Labs"));
+    }
+}
