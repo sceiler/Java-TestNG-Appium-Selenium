ef3b2335433cbc065d77f0f98945d6358e348b0a
Yi Min Yang <yi-min.yang@saucelabs.com>
2022-07-21 20:58:03 +0200

    I CAN HAZ COMMENTZ.

--- src/test/java/com/saucelabs/yy/Utility/RemoteWebDriverExtended.java
+++ src/test/java/com/saucelabs/yy/Utility/RemoteWebDriverExtended.java
@@ -1,15 +1,10 @@
 package com.saucelabs.yy.Utility;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.MutableCapabilities;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.AbstractDriverOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
-import java.time.Duration;
 
 public class RemoteWebDriverExtended extends RemoteWebDriver {
 
@@ -21,9 +16,14 @@ public class RemoteWebDriverExtended extends RemoteWebDriver {
         super(driverURL, capabilities);
     }
 
-    @Override
-    public WebElement findElement(By locator) {
-        new WebDriverWait(this, Duration.ofSeconds(30), Duration.ofMillis(250)).until(ExpectedConditions.elementToBeClickable(locator));
-        return super.findElement(locator);
-    }
+//    @Override
+//    public WebElement findElement(By locator) {
+//
+//        if (this.findElement(locator).isEnabled() && this.findElement(locator).isDisplayed()) {
+//            return super.findElement(locator);
+//        } else {
+//            new WebDriverWait(this, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(locator));
+//            return super.findElement(locator);
+//        }
+//    }
 }
