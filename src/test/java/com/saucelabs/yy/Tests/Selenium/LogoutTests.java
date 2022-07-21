1a0fd6635268e52ece4c696e3f2a897013a6d07d
Yi Min Yang <yi-min.yang@saucelabs.com>
2022-07-21 21:38:47 +0200

    s/import/include/

--- src/test/java/com/saucelabs/yy/Tests/Selenium/LogoutTests.java
+++ src/test/java/com/saucelabs/yy/Tests/Selenium/LogoutTests.java
@@ -1,6 +1,7 @@
 package com.saucelabs.yy.Tests.Selenium;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.lang.reflect.Method;
@@ -11,17 +12,10 @@ public class LogoutTests extends TestBase {
     @Test(dataProvider = "Browsers")
     public void passingLogout(String browser, String version, String os, Method method) throws MalformedURLException {
         createDriver(browser, version, os, method.getName());
-        annotate("Open saucedemo.com");
-        getRemoteWebDriver().get("https://www.saucedemo.com");
-        annotate("Sending valid username");
-        getRemoteWebDriver().findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
-        annotate("Sending valid password");
-        getRemoteWebDriver().findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
-        annotate("Clicking login button");
-        getRemoteWebDriver().findElement(By.cssSelector("#login-button")).click();
         annotate("Open menu");
         getRemoteWebDriver().findElement(By.cssSelector("#react-burger-menu-btn")).click();
         annotate("Click logout button");
         getRemoteWebDriver().findElement(By.cssSelector("#logout_sidebar_link")).click();
+        Assert.assertTrue(getRemoteWebDriver().findElement(By.cssSelector("#login-button")).isDisplayed());
     }
 }
